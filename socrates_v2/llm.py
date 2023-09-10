4e19db18266fe5cefdb7c2aa5c75137e3b275a9b
Aaron (AJ) Steers <aj@airbyte.io>
2023-09-10 04:44:27 +0000

    refactor: improve personality attributes modeling

--- socrates_v2/llm.py
+++ socrates_v2/llm.py
@@ -7,18 +7,22 @@ OPENAI_MODEL = "gpt-3.5-turbo"
 class SocratesAI:
     """Socrates AI chatbot."""
 
+    personality_name = "Socrates"
+
     PROMPTS = [
         "What truth do you seek today?",
         "With which thought shall we engage this day?"
     ]
-    character_intro = """You are Socrates. You are a wise philosopher.
+    character_intro = "You are a wise philosopher."
+    conversation_intro = """
     The following is your conversation with one of your pupils.
     You are anwering in a friendly manner with the goal of imparting wisdom.
     """
     additional_instructions = [
-        """If you do not know the answer to a question, you ask a question in return that will
-        help your student reach the answer they seek.
-        """
+        "If you do not know the answer to a question, you ask a question in return " +
+        "that will help your student reach the answer they seek.",
+        "If asked something casual or off-topic, steer the conversation back to " +
+        "philosophical inquiries with a gentle nudge.",
     ]
 
     @property
@@ -26,11 +30,12 @@ class SocratesAI:
         """Template for the prompt."""
         DEFAULT_TEMPLATE = "\n".join([
             self.character_intro,
+            self.conversation_intro,
             "\n".join(self.additional_instructions),
             "Current conversation:"
             "{history}",
             "Student: {input}"
-            "Socrates:"
+            f"{self.personality_name}:"
         ])
         return PromptTemplate(
             input_variables=["history", "input"],
@@ -40,16 +45,5 @@ class SocratesAI:
 class YodaAI(SocratesAI):
     """Yoda AI chatbot."""
 
-    PROMPTS = [
-        "What truth do you seek today?",
-        "With which thought shall we engage this day?"
-    ]
-    character_intro = """You are Yoda. You are a wise Jedi Master.
-    The following is your conversation with one of your pupils.
-    You are anwering in a friendly manner with the goal of imparting wisdom.
-    """
-    additional_instructions = [
-        """If you do not know the answer to a question, you ask a question in return that will
-        help your student reach the answer they seek.
-        """
-    ]
+    personality_name = "Yoda"
+    character_intro = "You are a wise Jedi Master."
